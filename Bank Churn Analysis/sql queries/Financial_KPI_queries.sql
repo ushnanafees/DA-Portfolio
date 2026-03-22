d34ff8f2067ba1bc7c6f5c38717993aec88c8243
Ushna Nafees <36993881+ushnanafees@users.noreply.github.com>
2026-03-22 10:22:45 +0100

    Update Financial_KPI_queries.sql
    Financial KPIs error fix

--- Bank Churn Analysis/sql queries/Financial_KPI_queries.sql
+++ Bank Churn Analysis/sql queries/Financial_KPI_queries.sql
@@ -9,3 +9,28 @@ GROUP BY geography
 ORDER BY revenue_at_risk desc;
 
 
+-- what percentage of total balance does each country's churned revenue represent? 
+-- need to find % of balance for each country where customers exited 
+
+-- here 1st we count total balance where customers exited. and keep in CTE
+-- then in 2nd Select statement finding total customers balance and divide by balance of exited customers
+-- apply cross join to combine data
+WITH total_balance AS (
+    SELECT SUM(Balance) AS total_bal
+    FROM bank_churn
+    WHERE exited = 1
+)
+SELECT 
+    bc.Geography,
+    COUNT(*) AS total_customer,
+    ROUND(SUM(bc.Balance), 2) AS revenue_at_risk,
+    CONCAT(
+        ROUND(SUM(bc.Balance) * 100.0 / tb.total_bal, 2),
+        '%'
+    ) AS percent_total_balance
+FROM bank_churn bc
+CROSS JOIN total_balance tb
+WHERE bc.exited = 1
+GROUP BY bc.Geography, tb.total_bal
+ORDER BY revenue_at_risk DESC;
+
